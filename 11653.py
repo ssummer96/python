5e6181764dd47fe2d8ac21d34306be0b55c04a6f
ssummer96 <pupu25866@gmail.com>
2022-03-21 01:21:08 +0900

    baekjoon_11653

---
+++ 11653.py
@@ -0,0 +1,27 @@
+n=int(input())
+
+# def fun(n):
+    
+#     if n==1:
+#         return
+    
+#     for i in range(2,n+1):
+#         if n%i==0:
+#             print(i)
+#             n=int(n/i)
+#             fun(n)        
+#             return
+
+# fun(n)
+
+x=2
+while n!=1:
+    if n%x==0:
+        n=n/x
+        print(x)
+    else : x+=1
+
+
+
+
+
